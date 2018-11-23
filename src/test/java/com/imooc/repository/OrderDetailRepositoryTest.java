06d89c6e2ae4dc341aabb37626f24a88d620ba96
haloer111 <3452499661@qq.com>
2018-11-23 23:22:40 +0800

    Create OrderDetailRepositoryTest.java

---
+++ src/test/java/com/imooc/repository/OrderDetailRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.imooc.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author gexiao
+ * @date 2018/11/23 下午 03:21
+ */
+public class OrderDetailRepositoryTest {
+    @Test
+    public void findByOrderId() throws Exception {
+    }
+
+}
